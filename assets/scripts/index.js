a5ca86652b9d09b2db9e6f945ce852501bf264b5
ND56 <ndrew56@gmail.com>
2018-03-08 12:16:54 -0500

    Built additional ui

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,10 +9,11 @@ $(() => {
 })
 
 $(() => {
+  $('#home-page').hide()
   $('#log-out-button').hide()
   $('#change-pw-button').hide()
-  $('#user-profile-header').hide()
-  $('#user-profile-page').hide()
+  // $('#user-profile-header').hide()
+  // $('#user-profile-page').hide()
   $('#register-form').on('submit', events.onSignUp)
   $('#log-in-form').on('submit', events.onLogIn)
   $('#log-out-button').on('click', events.onLogOut)
